d3d00576c3ffa78c86d2ee100a0c1a2727ec5fa7
sahile-jhalak <sahilansarishb@gmail.com>
2024-02-21 22:59:43 +0530

    Stop Watch commit

---
+++ script.js
@@ -0,0 +1,51 @@
+let timerDisplay = document.querySelector('.timerDisplay');
+let stopBtn = document.getElementById('stopBtn');
+let startBtn = document.getElementById('startBtn');
+let resetBtn = document.getElementById('resetBtn');
+
+const newLocal = 0;
+let msec = newLocal;
+const newLocal_1 = 0;
+let secs = newLocal_1;
+const newLocal_2 = 0;
+let mins = newLocal_2;
+
+let timerId = null;
+
+startBtn.addEventListener('click', function(){
+    if(timerId !== null){
+        clearInterval(timerId);
+    }
+    timerId = setInterval(startTimer, 10);
+});
+
+stopBtn.addEventListener('click', function(){
+    clearInterval(timerId);
+});
+
+resetBtn.addEventListener('click', function(){
+    clearInterval(timerId);
+    timerDisplay.innerHTML = `00 : 00 : 00`;
+    const newLocal = 0;
+    msec = secs = mins = newLocal;
+});
+
+function startTimer(){
+    msec++;
+    if(msec == 100){
+        msec = 0;
+        secs++;
+        if(secs == 60){
+            secs = 0;
+mins++;
+        }
+    }
+
+    let msecString = msec < 10 ? `0${msec}` : msec;
+    let secsString = secs < 10 ? `0${secs}` : secs;
+    let minsString = mins < 10 ? `0${mins}` : mins;
+    
+
+    timerDisplay.innerHTML = `${minsString} : ${secsString} : ${msecString}`;
+
+}
